6ee2a645043153237b8ec399d70cbc69d46b9cb2
syntaxerr-or <risky.moe@gmail.com>
2014-10-06 08:19:44 +0700

    #3 Commit
    Application add

---
+++ application/modules/dashboard/controllers/dashboard.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Dashboard extends MX_Controller {
+
+	function __construct() {
+        parent::__construct();
+    }
+
+	public function index()
+	{
+        $data['pagetitle']	= 'Dashboard';
+        $data['time'] 		= date('Y/m/d');
+        $js['js']			= array('vmap','flot','pulsate','daterange','gritter','fullcalendar','piechart','sparkline');
+        $js['custom']		= array('jvmap');
+		$this->template->display('dashboard', $data, $js);
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
